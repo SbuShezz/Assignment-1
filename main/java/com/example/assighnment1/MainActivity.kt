6063c89cc55c652aafb8c2b142893ca38d7a3dce
SbuShezz <shezisbonelo158@icloud.com>
2026-03-25 13:48:37 +0200

    Add files via upload

---
+++ main/java/com/example/assighnment1/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.assighnment1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var editTextTimeOfDay: EditText
+    private lateinit var buttonGetSuggestion: Button
+    private lateinit var textViewSuggestion: TextView
+    private lateinit var buttonReset: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+            // initialize views
+            editTextTimeOfDay=findViewById<R.id.edit>()
+        }
+        }
+
+            }
+
+
+    }
+
+    }
